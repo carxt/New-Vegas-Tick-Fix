46ea781ca50d2ac210f2b1aeec8420b488fd90fd
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2025-04-18 13:54:54 +0200

    Move timer hook code to the actual timer update
    Clamp value is more correct now

---
+++ nvtf/internal/Game/Bethesda/BSTimer.cpp
@@ -0,0 +1,6 @@
+#include "BSTimer.hpp"
+
+// GAME - 0xAA4EE0
+void BSTimer::Update(uint32_t auiTime) {
+	ThisCall(0xAA4EE0, this, auiTime);
+}
--- nvtf/internal/Game/Bethesda/BSTimer.hpp
+++ nvtf/internal/Game/Bethesda/BSTimer.hpp
@@ -2,14 +2,17 @@
 
 class BSTimer {
 public:
-	bool		bDisableCounter;
+	uint8_t		ucDisableCounter;
 	float		fClamp;
 	float		fClampRemainder;
 	float		fDelta;
 	float		fRealTimeDelta;
-	uint32_t	uiMsPassed;
-	uint32_t	uiUnk18;
+	uint32_t	uiLastTime;
+	uint32_t	uiFirstTime;
 	bool		bIsChangeTimeMultSlowly;
+	bool		bSameFrameDelta;
+
+	void Update(uint32_t auiTime);
 };
 
 ASSERT_SIZE(BSTimer, 0x20);
